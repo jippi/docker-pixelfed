13f3476d58c0956340d6711421f0a1cf686e7d3d
Christian Winther <jippignu@gmail.com>
2024-06-03 23:39:21 +0200

    more e2e

--- images/pixelfed/tests/playwright/e2e.spec.ts
+++ images/pixelfed/tests/playwright/e2e.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test'
 test('page should load without errors', async ({ page }) => {
     await page.goto('/')
 
-    expect(page).toHaveTitle(/ngrok-free/)
+    expect(page).toHaveTitle("docker-pixelfed e2e")
 })
