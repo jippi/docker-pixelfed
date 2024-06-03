0983095e39c5f934e2481f76bf2fee61b95271ae
Christian Winther <jippignu@gmail.com>
2024-06-04 01:04:45 +0200

    test apache and nginx e2e

--- images/pixelfed/tests/e2e.sh
+++ images/pixelfed/tests/e2e.sh
@@ -2,24 +2,27 @@
 
 set -o errexit -o nounset -o pipefail
 
-# copy .env file
+echo "==> Copying .env file"
 cp -v .env.docker .env
 
-# fix .env permissions
+echo "==> Changing .env permissions"
 chmod -v 0777 .env
 
-# start ngrok
+echo "==> Starting ngrok"
 docker run --quiet --detach --name ngrok --env NGROK_AUTHTOKEN --net=host ngrok/ngrok http 8080
 
-# find ngrok domain
+echo "==> Following ngrok logs"
+docker logs ngrok -f &
+
+echo "==> Finding ngrok tunnel public URL"
 domain="$(curl --retry-all-errors --fail --retry 60 --retry-max-time 60 http://127.0.0.1:4040/api/tunnels | jq -r ".tunnels[0].public_url")"
-echo "domain=${domain}"
+echo "OK: ${domain}"
 
-# remove https:// prefix for APP_DOMAIN
+echo "==> Finding ngrok tunnel public host (domain without https://)"
 app_domain=${domain#https://*}
-echo "app_domain=${domain}"
+echo "OK: ${app_domain}"
 
-# configure .env file
+echo "==> Reconfiguring .env file for testing"
 scripts/dottie set \
     APP_DOMAIN="${app_domain}" \
     APP_NAME="docker-pixelfed e2e" \
@@ -34,18 +37,18 @@ scripts/dottie set \
     INSTANCE_CONTACT_EMAIL="github@example.com" \
     OAUTH_ENABLED="true"
 
-# Start Pixelfed containers in the background
-docker compose up -d --no-build
+echo "==> Starting Pixelfed containers"
+docker compose up -d --no-build --quiet
 
-# Install npm dependencies
+echo "==> Starting npm dependencies"
 npm ci
 
-# Install playwright depdendencies
+echo "==> Install playwright depdendencies"
 npx playwright install chromium --with-deps
 
-# Wait for the site to come up, while streaming the logs
+echo "==> Wait for the site to come up, while streaming the logs"
 curl --header "ngrok-skip-browser-warning: true" --retry-delay 1 --retry 120 --retry-all-errors --fail -o /dev/null "${domain}"
 
-# Run playwright tests
+echo "==> Run playwright tests"
 export E2E_URL="${domain}"
 exec npx playwright test --config images/pixelfed/playwright.config.ts
