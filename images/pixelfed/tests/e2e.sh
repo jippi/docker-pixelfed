585cff98034bd066c0714e27d16d6b9db1224f04
Christian Winther <jippignu@gmail.com>
2024-06-04 01:17:20 +0200

    test apache and nginx e2e

--- images/pixelfed/tests/e2e.sh
+++ images/pixelfed/tests/e2e.sh
@@ -47,7 +47,7 @@ echo "==> Install playwright depdendencies"
 npx playwright install chromium --with-deps
 
 echo "==> Wait for the site to come up, while streaming the logs"
-curl --header "ngrok-skip-browser-warning: true" --retry-delay 1 --retry 120 --retry-all-errors --fail -o /dev/null "${domain}"
+curl --header "ngrok-skip-browser-warning: true" --retry-delay 1 --retry 60 --retry-max-time 60 --retry-all-errors --fail -o /dev/null "${domain}"
 
 echo "==> Run playwright tests"
 export E2E_URL="${domain}"
