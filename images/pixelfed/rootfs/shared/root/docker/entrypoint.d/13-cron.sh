1b0d213f804a2525f34b2e3213d556df0592f1f9
Christian Winther <jippignu@gmail.com>
2024-06-30 20:26:26 +0200

    fix: no longer copy cron file  manually

--- images/pixelfed/rootfs/shared/root/docker/entrypoint.d/13-cron.sh
+++ images/pixelfed/rootfs/shared/root/docker/entrypoint.d/13-cron.sh
@@ -6,9 +6,6 @@ source "${ENTRYPOINT_ROOT}/helpers.sh"
 
 entrypoint-set-script-name "$0"
 
-# copy pixelfed cron file
-run-as-current-user cp --verbose --force "${ENTRYPOINT_ROOT}/cron.d/pixelfed" "/etc/cron.d/pixelfed"
-
 # ensure cron file(s) has correct permissions
 run-as-current-user chmod --verbose 0644 "/etc/cron.d/*"
 
