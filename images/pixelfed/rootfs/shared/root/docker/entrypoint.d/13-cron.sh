baf76ac1ea9063abcdfe5282cccdaa69e36eeade
Christian Winther <jippignu@gmail.com>
2024-06-30 20:33:57 +0200

    fix: cron path

--- images/pixelfed/rootfs/shared/root/docker/entrypoint.d/13-cron.sh
+++ images/pixelfed/rootfs/shared/root/docker/entrypoint.d/13-cron.sh
@@ -7,6 +7,6 @@ source "${ENTRYPOINT_ROOT}/helpers.sh"
 entrypoint-set-script-name "$0"
 
 # ensure cron file(s) has correct permissions
-run-as-current-user chmod --verbose 0644 "/etc/cron.d/*"
+run-as-current-user chmod --verbose 0644 /etc/cron.d/*
 
 exit 0
