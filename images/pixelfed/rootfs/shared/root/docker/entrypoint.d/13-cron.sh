b2dbf4d023cda7d91003cee6500c615603778291
Christian Winther <jippignu@gmail.com>
2024-06-30 20:04:13 +0200

    fix: refine bash path for cron

--- images/pixelfed/rootfs/shared/root/docker/entrypoint.d/13-cron.sh
+++ images/pixelfed/rootfs/shared/root/docker/entrypoint.d/13-cron.sh
@@ -6,7 +6,10 @@ source "${ENTRYPOINT_ROOT}/helpers.sh"
 
 entrypoint-set-script-name "$0"
 
-run-as-current-user cp --force "${ENTRYPOINT_ROOT}/cron.d/pixelfed" "/etc/cron.d/pixelfed"
-run-as-current-user chmod --verbose 0644 "/etc/cron.d/pixelfed"
+# copy pixelfed cron file
+run-as-current-user cp --verbose --force "${ENTRYPOINT_ROOT}/cron.d/pixelfed" "/etc/cron.d/pixelfed"
+
+# ensure cron file(s) has correct permissions
+run-as-current-user chmod --verbose 0644 "/etc/cron.d/*"
 
 exit 0
