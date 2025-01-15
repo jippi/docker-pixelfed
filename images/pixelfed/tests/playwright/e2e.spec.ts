9ea6078cdc33c0155dedd17cfe95724d477a99d7
Christian Winther <jippignu@gmail.com>
2025-01-15 18:54:43 +0000

    more e2e tests

--- images/pixelfed/tests/playwright/e2e.spec.ts
+++ images/pixelfed/tests/playwright/e2e.spec.ts
@@ -1,9 +1,24 @@
 import { test, expect } from '@playwright/test'
 
 test('page should load without errors', async ({ page }) => {
-    console.error("E2E_URL", process.env.E2E_URL)
-
     await page.goto('/')
 
     expect(page).toHaveTitle("docker-pixelfed e2e")
 })
+
+test('/api/nodeinfo/2.0.json', async ({ page }) => {
+    const response = await page.get('/api/nodeinfo/2.0.json')
+    const json = JSON.parse(await response.text())
+
+    expect(json.nodeName).toBe("docker-pixelfed e2e")
+    expect(json.software.name).toBe("pixelfed")
+    expect(json.usage.users.total).toBe(0)
+})
+
+test('/api/v1/instance', async ({ page }) => {
+    const response = await page.get('/api/v1/instance')
+    const json = JSON.parse(await response.text())
+
+    expect(json.email).toBe("github@example.com")
+})
+
