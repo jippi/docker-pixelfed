9e0da59590e25aec485760283ee1974ebd7e030b
Christian Winther <jippignu@gmail.com>
2025-01-15 16:59:40 +0000

    connect e2e to the correct HTTP port

--- images/pixelfed/tests/e2e.sh
+++ images/pixelfed/tests/e2e.sh
@@ -37,8 +37,8 @@ echo "==> Install playwright depdendencies"
 npx playwright install chromium --with-deps
 
 echo "==> Wait for the site to come up, while streaming the logs"
-curl --retry-delay 1 --retry 120 --retry-max-time 120 --retry-all-errors --fail -o /dev/null "http://${app_domain}"
+curl --retry-delay 1 --retry 120 --retry-max-time 120 --retry-all-errors --fail -o /dev/null "http://${app_domain}:8080"
 
 echo "==> Run playwright tests"
-export E2E_URL="http://${app_domain}"
+export E2E_URL="http://${app_domain}:8080"
 exec npx playwright test --config images/pixelfed/playwright.config.ts
