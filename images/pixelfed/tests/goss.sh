cf24e97c67d10b647586a362a48560854c52dc03
Christian Winther <jippignu@gmail.com>
2025-01-16 14:51:13 +0000

    chore: document why 'ENTRYPOINT_SKIP_SCRIPTS' is used in GOSS

--- images/pixelfed/tests/goss.sh
+++ images/pixelfed/tests/goss.sh
@@ -2,11 +2,19 @@
 
 set -o errexit -o nounset -o pipefail
 
+# We skip these entrypoints as they require other containers (like DB or Redis)
+# which won't be running during GOSS testing
+export ENTRYPOINT_SKIP_SCRIPTS="02-check-config.sh,11-first-time-setup.sh,12-migrations.sh"
+
+export EXPECTED_PHP_VERSION=${DOCKER_APP_PHP_VERSION:?missing}
+
+export PHP_BASE_TYPE=${PHP_BASE_TYPE:?missing}
+
 for tag in ${TAGS:?missing}; do
     dgoss run \
         -v "./.env.testing:/var/www/.env" \
-        -e "EXPECTED_PHP_VERSION=${DOCKER_APP_PHP_VERSION:?missing}" \
-        -e "PHP_BASE_TYPE=${PHP_BASE_TYPE?:missing}" \
-        -e ENTRYPOINT_SKIP_SCRIPTS="02-check-config.sh,11-first-time-setup.sh,12-migrations.sh" \
+        -e ENTRYPOINT_SKIP_SCRIPTS \
+        -e EXPECTED_PHP_VERSION \
+        -e PHP_BASE_TYPE \
         "$tag"
 done
