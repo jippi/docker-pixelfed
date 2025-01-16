dbec1e282e3f6fd8ba72655976dead790657ef51
Christian Winther <jippignu@gmail.com>
2025-01-16 13:45:15 +0000

    tweak goss testing

--- images/pixelfed/tests/goss.sh
+++ images/pixelfed/tests/goss.sh
@@ -7,5 +7,6 @@ for tag in ${TAGS:?missing}; do
         -v "./.env.testing:/var/www/.env" \
         -e "EXPECTED_PHP_VERSION=${DOCKER_APP_PHP_VERSION:?missing}" \
         -e "PHP_BASE_TYPE=${PHP_BASE_TYPE?:missing}" \
+        -e ENTRYPOINT_SKIP_SCRIPTS="02-check-config.sh,11-first-time-setup.sh,12-migrations.sh" \
         "$tag"
 done
