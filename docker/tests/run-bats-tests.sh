5690286511ff628aceef1d9224b3136487f88475
Christian Winther <jippignu@gmail.com>
2025-02-24 13:29:52 +0100

    fix: support POST_MAX_SIZE over 1GB by forcing output values to be M (#164)
    Fixes #163

--- docker/tests/run-bats-tests.sh
+++ docker/tests/run-bats-tests.sh
@@ -5,4 +5,17 @@ declare project_root="${PWD}"
 
 command -v git &>/dev/null && project_root=$(git rev-parse --show-toplevel)
 
-exec docker run -it -v "${project_root}:/var/www" bats/bats:latest /var/www/docker/tests/bats
+docker build -t local-bats -f "${project_root}/docker/tests/bats.Dockerfile" .
+
+# [docker run] flags
+declare -a docker_flags=(
+    --rm
+    --env TERM
+    --env COLORTERM
+    --volume "${project_root}:/var/www"
+)
+
+# if the shell is interactive, attach the tty and stdin to the container
+tty -s && [ $# -eq 0 ] && docker_flags+=(--interactive --tty)
+
+exec docker run "${docker_flags[@]}" local-bats /var/www/docker/tests/bats
